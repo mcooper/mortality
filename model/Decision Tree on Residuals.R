c7e37121c9354ef3cd5d1014b5a7a8b1599f708a
mcooper <mw.coop.r@gmail.com>
2019-06-25 21:37:44 -0400

    retry glm residuals

--- model/Decision Tree on Residuals.R
+++ model/Decision Tree on Residuals.R
@@ -1,11 +1,25 @@
 library(tidyverse)
+library(lme4)
 
-data <- read.csv('/home/mattcoop/child-months/Mortality-combined.csv')
-
-data$mortality <- !data$alive
+data <- read_csv('/home/mattcoop/child-months/Mortality-combined.csv') %>%
+  select(alive, age, mother_years_ed, mothers_age, birth_order, male, cc, code) %>%
+  na.omit %>%
+  mutate(mortality = !alive,
+         survey_code = substr(code, 1, 4))
 
 mod <- glm(mortality ~ age + mother_years_ed + mothers_age + birth_order + male, 
            family='binomial', data=data)
 
-data$res <- resdiuals(mod)
+data$glm_res <- resdiuals(mod)
+
+system('/home/mattcoop/telegram.sh "Done with GLM Residuals"')
+
+mod2 <- glmer(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + (1|cc) + (1|survey_code), 
+           family='binomial', data=data)
+
+data$glmm_res <- resdiuals(mod2)
+
+system('/home/mattcoop/telegram.sh "Done with GLMM Residuals"')
+
+write.csv(data, '/home/mattcoop/child-months/Mortality-combined-residuals.csv')
 

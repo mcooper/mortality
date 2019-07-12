657ee725b92a9737a1ea72ef8f62e81e61f078c4
mcooper <mw.coop.r@gmail.com>
2019-07-12 20:52:03 +0200

    tweaks to gam script

--- model/gam.R
+++ model/gam.R
@@ -7,10 +7,9 @@ data <- read_csv('/home/mattcoop/child-months/Mortality-combined.csv')
 data$mortality <- !data$alive
 
 cl <- makeCluster(4, outfile = '')
-registerDoParallel(cl)
 
 data <- data %>%
-  select(mortality, age, mother_years_ed, mothers_age, birth_order, male, spei3, spei12, spei36) %>%
+  select(mortality, age, mother_years_ed, mothers_age, birth_order, male, spei3, spei36) %>%
   na.omit %>%
   filter(!is.infinite(spei3) & !is.infinite(spei12) & !is.infinite(spei36))
 
@@ -30,11 +29,5 @@ save(mod36, file='~/mod-results/mod36_new.Rdata')
 system('/home/matt/telegram.sh "36 Smooths Done!"')
 
 
-mod12 <- bam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + s(spei12), 
-             family='binomial', data=data, cluster=cl)
-
-save(mod12, file='~/mod-results/mod12_new.Rdata')
-
-system('/home/matt/telegram.sh "12 Smooths Done!"')
 
 

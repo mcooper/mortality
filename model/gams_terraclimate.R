43b2f5c07bef5ebcb32e7c5146f61c0bda6b4e45
mcooper <mw.coop.r@gmail.com>
2020-09-06 04:25:21 +0000

    use correct paths, remove infinite values

--- model/gams_terraclimate.R
+++ model/gams_terraclimate.R
@@ -5,168 +5,174 @@ library(data.table)
 
 cl <- makeCluster(8, outfile = '')
 
-setwd('/home/mattcoop/mortalityblob/')
+setwd('/home/mattcoopcoop/mortalityblob/')
 
 #############################
 # Analyze full Subset
 #############################
 
-data <- fread('mortality-dhs/Mortality-subset-full.csv')
+data <- fread('mortality-dhs/Mortality-subset-full.csv') %>%
+  filter(!is.infinite(spei1) & !is.infinite(spei3) & !is.infinite(spei36))
 
 ###wbgtZ
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(wbgtZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/wbgtZ_full.Rdata')
-system('/home/matt/telegram.sh "wbgtZ done!"')
+save(mod, file='mod-results/terraclimate/wbgtZ_full.Rdata')
+system('/home/mattcoop/telegram.sh "wbgtZ done!"')
 
 ###tmax
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
-           s(tmax1) + offset(offset),
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
+           s(tmaxZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/tmax_full.Rdata')
-system('/home/matt/telegram.sh "tmax done!"')
+save(mod, file='mod-results/terraclimate/tmax_full.Rdata')
+system('/home/mattcoop/telegram.sh "tmax done!"')
 
 ###spei1
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei1_full.Rdata')
-system('/home/matt/telegram.sh "spei1 done!"')
+save(mod, file='mod-results/terraclimate/spei1_full.Rdata')
+system('/home/mattcoop/telegram.sh "spei1 done!"')
 
 ###spei3
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei3) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei3_full.Rdata')
-system('/home/matt/telegram.sh "spei3 done!"')
+save(mod, file='mod-results/terraclimate/spei3_full.Rdata')
+system('/home/mattcoop/telegram.sh "spei3 done!"')
 
 ###spei36
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei36) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei36_full.Rdata')
-system('/home/matt/telegram.sh "spei36 done!"')
+save(mod, file='mod-results/terraclimate/spei36_full.Rdata')
+system('/home/mattcoop/telegram.sh "spei36 done!"')
 
 #############################
 # Analyze 2yo Subset
 #############################
 
-data <- fread('mortality-dhs/Mortality-subset-2yo.csv')
+data <- fread('mortality-dhs/Mortality-subset-2yo.csv') %>%
+  filter(!is.infinite(spei1) & !is.infinite(spei3) & !is.infinite(spei36))
 
 ###wbgtZ
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(wbgtZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/wbgtZ_2yo.Rdata')
-system('/home/matt/telegram.sh "wbgtZ done!"')
+save(mod, file='mod-results/terraclimate/wbgtZ_2yo.Rdata')
+system('/home/mattcoop/telegram.sh "wbgtZ done!"')
 
 ###tmax
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
-           s(tmax1) + offset(offset),
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
+           s(tmaxZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/tmax_2yo.Rdata')
-system('/home/matt/telegram.sh "tmax done!"')
+save(mod, file='mod-results/terraclimate/tmax_2yo.Rdata')
+system('/home/mattcoop/telegram.sh "tmax done!"')
 
 ###spei1
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei1_2yo.Rdata')
-system('/home/matt/telegram.sh "spei1 done!"')
+save(mod, file='mod-results/terraclimate/spei1_2yo.Rdata')
+system('/home/mattcoop/telegram.sh "spei1 done!"')
 
 ###spei3
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei3) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei3_2yo.Rdata')
-system('/home/matt/telegram.sh "spei3 done!"')
+save(mod, file='mod-results/terraclimate/spei3_2yo.Rdata')
+system('/home/mattcoop/telegram.sh "spei3 done!"')
 
 ###spei36
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei36) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei36_2yo.Rdata')
-system('/home/matt/telegram.sh "spei36 done!"')
+save(mod, file='mod-results/terraclimate/spei36_2yo.Rdata')
+system('/home/mattcoop/telegram.sh "spei36 done!"')
 
 #############################
 # Analyze 5yb Subset
 #############################
 
-data <- fread('mortality-dhs/Mortality-subset-5yb.csv')
+data <- fread('mortality-dhs/Mortality-subset-5yb.csv') %>%
+  filter(!is.infinite(spei1) & !is.infinite(spei3) & !is.infinite(spei36))
 
 ###wbgtZ
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(wbgtZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/wbgtZ_5yb.Rdata')
-system('/home/matt/telegram.sh "wbgtZ done!"')
+save(mod, file='mod-results/terraclimate/wbgtZ_5yb.Rdata')
+system('/home/mattcoop/telegram.sh "wbgtZ done!"')
 
 ###tmax
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
-           s(tmax1) + offset(offset),
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
+           s(tmaxZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/tmax_5yb.Rdata')
-system('/home/matt/telegram.sh "tmax done!"')
+save(mod, file='mod-results/terraclimate/tmax_5yb.Rdata')
+system('/home/mattcoop/telegram.sh "tmax done!"')
 
 ###spei1
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei1_5yb.Rdata')
-system('/home/matt/telegram.sh "spei1 done!"')
+save(mod, file='mod-results/terraclimate/spei1_5yb.Rdata')
+system('/home/mattcoop/telegram.sh "spei1 done!"')
 
 ###spei3
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei3) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei3_5yb.Rdata')
-system('/home/matt/telegram.sh "spei3 done!"')
+save(mod, file='mod-results/terraclimate/spei3_5yb.Rdata')
+system('/home/mattcoop/telegram.sh "spei3 done!"')
 
 ###spei36
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei36) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei36_5yb.Rdata')
-system('/home/matt/telegram.sh "spei36 done!"')
+save(mod, file='mod-results/terraclimate/spei36_5yb.Rdata')
+system('/home/mattcoop/telegram.sh "spei36 done!"')
 
 #############################
 # Analyze 5yb2yo Subset
 #############################
 
-data <- fread('mortality-dhs/Mortality-subset-5yb2yo.csv')
+data <- fread('mortality-dhs/Mortality-subset-5yb2yo.csv') %>%
+  filter(!is.infinite(spei1) & !is.infinite(spei3) & !is.infinite(spei36))
 
 ###wbgtZ
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(wbgtZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/wbgtZ_5yb2yo.Rdata')
-system('/home/matt/telegram.sh "wbgtZ done!"')
+save(mod, file='mod-results/terraclimate/wbgtZ_5yb2yo.Rdata')
+system('/home/mattcoop/telegram.sh "wbgtZ done!"')
 
 ###tmax
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
-           s(tmax1) + offset(offset),
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
+           s(tmaxZ1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/tmax_5yb2yo.Rdata')
-system('/home/matt/telegram.sh "tmax done!"')
+save(mod, file='mod-results/terraclimate/tmax_5yb2yo.Rdata')
+system('/home/mattcoop/telegram.sh "tmax done!"')
 
 ###spei1
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei1) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei1_5yb2yo.Rdata')
-system('/home/matt/telegram.sh "spei1 done!"')
+save(mod, file='mod-results/terraclimate/spei1_5yb2yo.Rdata')
+system('/home/mattcoop/telegram.sh "spei1 done!"')
 
 ###spei3
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei3) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei3_5yb2yo.Rdata')
-system('/home/matt/telegram.sh "spei3 done!"')
+save(mod, file='mod-results/terraclimate/spei3_5yb2yo.Rdata')
+system('/home/mattcoop/telegram.sh "spei3 done!"')
 
 ###spei36
-mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + 
+mod <- gam(mortality ~ age + mother_years_ed + mothers_age + birth_order + male + date + 
            s(spei36) + offset(offset),
            family='binomial', data=data, cluster=cl)
-save(mod, file='mod-results/spei36_5yb2yo.Rdata')
-system('/home/matt/telegram.sh "spei36 done!"')
+save(mod, file='mod-results/terraclimate/spei36_5yb2yo.Rdata')
+system('/home/mattcoop/telegram.sh "spei36 done!"')
+
+

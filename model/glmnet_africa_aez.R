0c817071d6c62b980f3c74b841f6459a1f5a8f15
mcooper <mw.coop.r@gmail.com>
2020-11-30 03:25:28 +0000

    more aez hdi models

--- model/glmnet_africa_aez.R
+++ model/glmnet_africa_aez.R
@@ -10,7 +10,7 @@ piece.formula <- function(var.name, knots) {
   formula.sign <- rep(" - ", length(knots))
   formula.sign[knots < 0] <- " + "
   paste(var.name, "+",
-        paste("I(pmax(", var.name, formula.sign, abs(knots), ", 0))",
+        paste("aez*I(pmax(", var.name, formula.sign, abs(knots), ", 0))",
               collapse = " + ", sep=""))
 }
 
@@ -29,87 +29,68 @@ data <- merge(data, aez)
 
 data$cc <- substr(data$code, 1, 2)
 data$year <- 1900 + floor((data$date - 1)/12)
-data$year <- as.character(floor(data$year/5)*5)
+data$year <- as.character(floor(data$year/5))
 
 #SPEI03 TerraClim
-mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + ",
+mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + aez*",
                           piece.formula("spei.tc.3", c(-1, 0, 1)), 
                          " + birth_order + male + cc*year")), data=data)
 mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei03.tc.afr.Rdata')
+save(mod, file='~/mortalityblob/glmnet/spei03.tc.afr.aez.Rdata')
 rm(mm)
 
 system('~/telegram.sh "Done with TerraClimate SPEI03"')
 
-#SPEI36 TerraClim
-mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + ",
+#SPEI24 TerraClim
+mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + aez*",
                           piece.formula("spei.tc.24", c(-1, 0, 1)), 
                          " + birth_order + male + cc*year")), data=data)
 mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei24.tc.afr.Rdata')
+save(mod, file='~/mortalityblob/glmnet/spei24.tc.afr.aez.Rdata')
 rm(mm)
 
 system('~/telegram.sh "Done with TerraClimate SPEI24"')
 
 #SPEI03 CHIRPS
-mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + ",
+mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + aez*",
                           piece.formula("spei.ch.3", c(-1, 0, 1)), 
                          " + birth_order + male + cc*year")), data=data)
 mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei03.ch.afr.Rdata')
+save(mod, file='~/mortalityblob/glmnet/spei03.ch.afr.aez.Rdata')
 rm(mm)
 
 system('~/telegram.sh "Done with CHRIPS SPEI03"')
 
-#SPEI36 CHIRPS
-mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + ",
+#SPEI24 CHIRPS
+mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + aez*",
                           piece.formula("spei.ch.24", c(-1, 0, 1)), 
                          " + birth_order + male + cc*year")), data=data)
 mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei24.ch.afr.Rdata')
+save(mod, file='~/mortalityblob/glmnet/spei24.ch.afr.aez.Rdata')
 rm(mm)
 
 system('~/telegram.sh "Done with CHRIPS SPEI24"')
 
-#SPEI03 TerraClim No Local
-mm <- sparse.model.matrix(formula(paste0("mortality ~ ",
+#SPEI0324 TerraClim
+mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + aez*",
                           piece.formula("spei.tc.3", c(-1, 0, 1)), 
-                         " + cc*year")), data=data)
-mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei03.tc.afr.noloc.Rdata')
-rm(mm)
-
-system('~/telegram.sh "Done with TerraClimate SPEI03"')
-
-#SPEI36 TerraClim No Local
-mm <- sparse.model.matrix(formula(paste0("mortality ~ ",
+                          " + aez*",
                           piece.formula("spei.tc.24", c(-1, 0, 1)), 
-                         " + cc*year")), data=data)
+                         " + birth_order + male + cc*year")), data=data)
 mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei24.tc.afr.noloc.Rdata')
+save(mod, file='~/mortalityblob/glmnet/spei0324.tc.afr.aez.Rdata')
 rm(mm)
 
-system('~/telegram.sh "Done with TerraClimate SPEI24"')
+system('~/telegram.sh "Done with TerraClimate SPEI0324"')
 
-#SPEI03 CHIRPS No Local
-mm <- sparse.model.matrix(formula(paste0("mortality ~ ",
+#SPEI0324 CHIRPS
+mm <- sparse.model.matrix(formula(paste0("mortality ~ age + mother_years_ed + mothers_age + aez*",
                           piece.formula("spei.ch.3", c(-1, 0, 1)), 
-                         " + cc*year")), data=data)
-mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei03.ch.afr.noloc.Rdata')
-rm(mm)
-
-system('~/telegram.sh "Done with CHRIPS SPEI03"')
-
-#SPEI36 CHIRPS No Local
-mm <- sparse.model.matrix(formula(paste0("mortality ~ ",
+                          " + aez*",
                           piece.formula("spei.ch.24", c(-1, 0, 1)), 
-                         " + cc*year")), data=data)
+                         " + birth_order + male + cc*year")), data=data)
 mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
-save(mod, file='~/mortalityblob/glmnet/spei24.ch.afr.noloc.Rdata')
+save(mod, file='~/mortalityblob/glmnet/spei0324.ch.afr.aez.Rdata')
 rm(mm)
 
-system('~/telegram.sh "Done with CHRIPS SPEI24"')
-
-
-
+system('~/telegram.sh "Done with CHRIPS SPEI0324"')
---
+++ model/glmnet_africa_aez_hdi.R
@@ -0,0 +1,56 @@
+library(data.table)
+library(glmnet)
+library(Matrix)
+library(dplyr)
+library(countrycode)
+
+setwd('~/mortalityblob/mortality-dhs/')
+
+piece.formula <- function(var.name, knots) {
+  formula.sign <- rep(" - ", length(knots))
+  formula.sign[knots < 0] <- " + "
+  paste(var.name, "+",
+        paste("I(pmax(", var.name, formula.sign, abs(knots), ", 0))",
+              collapse = " + ", sep=""))
+}
+
+data <- fread('Mortality-combined.csv') %>%
+  filter(!is.infinite(spei.tc.1), !is.infinite(spei.tc.2), !is.infinite(spei.tc.3), 
+         !is.infinite(spei.tc.6), !is.infinite(spei.tc.12), !is.infinite(spei.tc.24), 
+         !is.infinite(spei.tc.36), !is.infinite(spei.tc.48),
+         countrycode(substr(code, 1, 2), 'dhs', 'region') == 'Sub-Saharan Africa') %>%
+  na.omit %>%
+  select(-spei.mm.1, -spei.mm.2, -spei.mm.3, -spei.mm.6, -spei.mm.12, -spei.mm.24, -spei.mm.36,
+         -spei.mm.48)
+
+aez <- read.csv('Mortality_AEZs.csv') %>%
+  select(-X, -latitude, -longitude, -earliest_date, -latest_date)
+
+hdi <- read.csv('Mortality_HDI.csv') %>%
+  select(-latitude, -longitude)
+
+data$year <- 1900 + floor((data$date - 1)/12)
+
+data <- merge(data, aez)
+data <- merge(data, hdi, by=c('code', 'year'))
+
+data$shdi <- 1 - data$shdi
+
+data$aez <- factor(data$aez)
+
+#TerraClim, SHDI, AEZ, 3 + 24
+mm <- sparse.model.matrix(mortality ~ 
+  aez*shdi*spei.tc.3 + aez*shdi*I(pmax(spei.tc.3 + 1, 0)) + 
+  aez*shdi*I(pmax(spei.tc.3 - 0, 0)) + aez*shdi*I(pmax(spei.tc.3 - 1, 0)) + 
+  aez*shdi*spei.tc.24 + aez*shdi*I(pmax(spei.tc.24 + 1, 0)) + 
+  aez*shdi*I(pmax(spei.tc.24 - 0, 0)) + aez*shdi*I(pmax(spei.tc.24 - 1, 0)) + 
+  healthindex + incindex + edindex, data=data)
+
+mod <- glmnet(mm, data$mortality, family='binomial', alpha=0, lambda=0)
+save(mod, file='~/mortalityblob/glmnet/spei0324.afr.aez.hdi.Rdata')
+
+mod <- cv.glmnet(mm, data$mortality, family='binomial', alpha=1)
+save(mod, file='~/mortalityblob/glmnet/spei0324.afr.aez.hdi.lasso.Rdata')
+
+system('~/telegram.sh "Donezeo!"')
+
